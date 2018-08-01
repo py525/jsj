baa3109a8b06785057171f26e6f9316c67bcfb20
py525 <2083464984@qq.com>
2018-08-01 21:03:21 +0800

    1

--- jsj.js
+++ jsj.js
@@ -8,8 +8,35 @@ $(function () {
    $('.E-a').eq(strs[0]).show();
    var title=['实验实训','科研设施','实验室','实习基地','学生实训室','科研|实验|实训活动'];
    var title1=['学生工作','学工概况','资助贷款','学习社团','就业指导','科技创新','团学活动'];
+   var title2=['评建专栏','评建工作','新闻动态','通知公告'];
    $("#changing").text(title[strs[0]]);
    $("#changing1").text(title1[strs[0]]);
+   $("#changing2").text(title2[strs[0]]);
+ if(strs==0)
+ {
+  page();
+ }
+ if(strs==1){
+  $('.s_body .pj_tz1').eq(0).show().siblings().hide();
+    $('.E-a .pjgz_tz').find('a').click(function(){
+       var swidx = $(this).index();
+       $('.s_body .pj_tz1').eq(swidx).show().siblings().hide();
+    })
+ }
+ if(strs==2){
+  $('.s_body .pj_tz2').eq(0).show().siblings().hide();
+    $('.E-a .pjgz_tz').find('a').click(function(){
+       var swidx = $(this).index();
+       $('.s_body .pj_tz2').eq(swidx).show().siblings().hide();
+    })
+ }
+ if(strs==3){
+  $('.s_body .pj_tz3').eq(0).show().siblings().hide();
+    $('.E-a .pjgz_tz').find('a').click(function(){
+       var swidx = $(this).index();
+       $('.s_body .pj_tz3').eq(swidx).show().siblings().hide();
+    })
+ }
    if(strs==6)
    {
     $('.s_body .s_tz').eq(0).show().siblings().hide();
@@ -169,9 +196,18 @@ $('.jw_nei_c ul li').click(function(){
 //教务教学字体切换
 $('.size').find('a').click(function(){
     var size=$(this).index();
-    var sizea=size*2+14;
-    $('.jw_cont p').css({'font-size':sizea});
-})
+    if(size==0)
+    {
+      $('.jw_cont p').css({'font-size':'18px'});
+    }
+    else if(size==1)
+    {
+      $('.jw_cont p').css({'font-size':'16px'});
+    }
+    else{
+      $('.jw_cont p').css({'font-size':'13px'});
+    }
+});
 
 //科学教研
 $('.r_content').eq(0).show().siblings().hide();
@@ -185,7 +221,86 @@ $('.c_p').find('.c_kd').mouseenter(function(){
 });
 
 
-    
+  $('.pj_body .pj_table').find('tr:even').css({'background-color':'rgba(221,221,221,0.18)'}) 
+function page(){
+ var pageSize = 4;    //每页显示的记录条数
+             var curPage=0;        //当前页
+             var lastPage;        //最后页
+             var direct=0;        //方向
+            var len;            //总行数
+            var page;            //总页数
+            var begin;
+            var end;    
+            $(document).ready(function display(){   
+                len =$(".pj_table tr").length ;    // 求这个表的总行数，剔除第一行介绍
+                page=len % pageSize==0 ? len/pageSize : Math.floor(len/pageSize)+1;//根据记录条数，计算页数
+                // alert("page==="+page);
+                curPage=1;    // 设置当前为第一页
+                displayPage(1);//显示第一页
+
+                document.getElementById("btn0").innerHTML="共9条 " + curPage + "/" + page + " 页";   // 显示当前多少页
+           $("#first").click(function firstPage(){    // 首页
+                    curPage=1;
+                    direct = 0;
+                    displayPage();
+                });
+                $(".t_prev").click(function frontPage(){    // 上一页
+                    direct=-1;
+                    displayPage();
+                });
+                $(".t_next").click(function nextPage(){    // 下一页
+                    direct=1;
+                    displayPage();
+                });
+                $(".last").click(function lastPage(){    // 尾页
+                    curPage=page;
+                    direct = 0;
+                    displayPage();
+                });
+                $("#gotopagebut").click(function changePage(){    // 转页
+                    curPage=document.getElementById("gopage").value * 1;
+                    if (!/^[1-9]\d*$/.test(curPage)) {
+                        alert("请输入正整数");
+                        return ;
+                    }
+                    if (curPage > page) {
+                        alert("超出数据页面");
+                        return ;
+                    }
+                    direct = 0;
+                    displayPage();
+                });
+            });
+            function displayPage(){
+                if(curPage <=1 && direct==-1){
+                    direct=0;
+                    alert("已经是第一页了");
+                    return;
+                } else if (curPage >= page && direct==1) {
+                    direct=0;
+                    alert("已经是最后一页了");
+                    return ;
+                }
+                lastPage = curPage;
+
+                // 修复当len=1时，curPage计算得0的bug
+                if (len > pageSize) {
+                    curPage = ((curPage + direct + len) % len);
+                } else {
+                    curPage = 1;
+                }    
+                document.getElementById("btn0").innerHTML="共9条 " + curPage + "/" + page + " 页";        // 显示当前多少页
+                begin=(curPage-1)*pageSize + 1;// 起始记录号
+                end = begin + 1*pageSize - 1;    // 末尾记录号     
+                if(end > len ) end=len;
+                $(".pj_table tr").hide();    // 首先，设置这行为隐藏
+                $(".pj_table tr").each(function(i){    // 然后，通过条件判断决定本行是否恢复显示
+                    if((i>=begin && i<=end) || i==0 )//显示begin<=x<=end的记录
+                        $(this).show();
+                });
+             }
+}
+/*跳页*/
 
 
 
